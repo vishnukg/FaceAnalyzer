9d0429686f04cbe66b481d9a0f646dd57c305793
Vishnu Ganesan <vishnukg@gmail.com>
2017-04-25 15:40:43 +1000

    Added CORS policy.

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,16 @@ namespace FaceDetectorApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
             // Add framework services.
             services.AddMvc();
             services.AddScoped<IClientProxy, HttpClientProxy>();
@@ -44,6 +54,9 @@ namespace FaceDetectorApi
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            // global policy - assign here or on each controller
+            app.UseCors("CorsPolicy");
 
             app.UseMvc();
         }
